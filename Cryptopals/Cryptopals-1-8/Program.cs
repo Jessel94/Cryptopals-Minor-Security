7e3a285798e60e6963f7bfcd4235987082f428b9
Unknown <jesselakerveld@hotmail.com>
2017-09-14 23:06:12 +0200

    Finished assignment 1-8

---
+++ Cryptopals/Cryptopals-1-8/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace Cryptopals_1_8
+{
+    using CryptopalsShared;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var path = Path.GetFullPath(Path.Combine(@"Data", @"..\..\..\"));
+            path = Path.GetFullPath(Path.Combine(path, "Data/8.txt"));
+
+            var ecbEncryptedString = EcbHelper.TryFindEcbEncryptedString(File.ReadAllLines(path));
+
+            Console.WriteLine(ecbEncryptedString);
+            Console.ReadKey();
+        }
+    }
+}
--- Cryptopals/CryptopalsShared/EcbHelper.cs
+++ Cryptopals/CryptopalsShared/EcbHelper.cs
@@ -1,5 +1,7 @@
 ﻿namespace CryptopalsShared
 {
+    using System.Collections.Generic;
+    using System.Linq;
     using System.Security.Cryptography;
 
     public class EcbHelper
@@ -17,6 +19,32 @@
 
             var ict = aes.CreateDecryptor();
             return ict.TransformFinalBlock(encryptedMessage, 0, encryptedMessage.Length);
+        }
+
+        public static string TryFindEcbEncryptedString(IEnumerable<string> base64EncodedString)
+        {
+            var repetitions = new Dictionary<string, int>();
+            foreach (var base64Encoded in base64EncodedString)
+            {
+                var hex = Base64Helper.Base64ToHex(base64Encoded);
+                var dictionary = new Dictionary<string, int>();
+
+                for (var i = 0; i < hex.Length / 32; i++)
+                {
+                    var bytes16 = hex.Substring(i * 32, 32);
+                    if (dictionary.ContainsKey(bytes16))
+                    {
+                        dictionary[bytes16]++;
+                    }
+                    else
+                    {
+                        dictionary.Add(hex.Substring(i * 32, 32), 1);
+                    }
+                }
+                repetitions.Add(base64Encoded, dictionary.Keys.Count);
+            }
+
+            return repetitions.First(kv => kv.Value == repetitions.Values.Min()).Key;
         }
     }
 }
